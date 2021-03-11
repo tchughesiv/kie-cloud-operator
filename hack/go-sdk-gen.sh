50225db7e6f3eeaab44615cda22aaca06a8e5e20
Tommy Hughes <tohughes@redhat.com>
2021-03-11 13:17:32 -0600

    [KIECLOUD-405] use openshift CA for truststore generation
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- hack/go-sdk-gen.sh
+++ hack/go-sdk-gen.sh
@@ -5,3 +5,9 @@ source ./hack/go-mod-env.sh
 operator-sdk generate k8s
 operator-sdk generate crds
 mv deploy/crds/app.kiegroup.org_kieapps_crd.yaml deploy/crds/kieapp.crd.yaml
+
+CSVVERSION=$(go run getversion.go -csv)
+for OLMDIR in deploy/olm-catalog/dev deploy/olm-catalog/test deploy/olm-catalog/prod
+do
+    cp -p deploy/crds/kieapp.crd.yaml ${OLMDIR}/${CSVVERSION}/manifests/
+done
